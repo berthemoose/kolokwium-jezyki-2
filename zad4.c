4f594063a7a47e18acc03921734d3e4187127939
Bartosz Przybyszewski <berthemoose@gmail.com>
2026-01-18 14:26:50 +0100

    done

--- zad4.c
+++ zad4.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#define N 5
 
-void grid_update(const int src[5][5], int dst[5][5]) {
-    
+void grid_update(const int src[N][N], int dest[N][N]) {
+ for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+     int sum = 0;
+     
+        // Sprawdz z góry
+        if (i > 0) {
+            sum += src[i-1][j];
+        }
+        // Sprawdz z dołu
+        if (i < N-1) {
+            sum += src[i+1][j];
+        }
+        // Sprawdz z lewej
+        if (j > 0) {
+            sum += src[i][j-1];
+        }
+        // Sprawdz z prawej
+        if (j < N-1) {
+            sum += src[i][j+1];
+        }
+        
+        if (src[i][j] > sum) {
+            dest[i][j] = 2;
+        } else if (src[i][j] == sum ) {
+            dest[i][j] = 1;
+        } else {
+            dest[i][j] = 0;
+        }
+    }
+ }
 }
